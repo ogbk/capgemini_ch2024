8af20adf3c349d5d2983c5f3fbc6870958bee0d3
chivchila <chivchila@gmail.com>
2024-04-08 14:41:40 +0100

    Add title and subtitle

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,26 @@ import CarbonChart from './charts/CarbonChart';
 
 function App() {
   return (
-    <div className='charts'>
-      <div className='chart-row'>
-        <CarbonChart metrics="memory/energy" title="Memory Energy"/>
-        <CarbonChart metrics="network/energy" title="Network Energy"/>
+    <>
+      <h1>Energy Consumption and Carbon Footprint for a Containerized LLM Service</h1>
+      <h2>The charts below present relevant parameters for the calculation of the SCI Score, in the span of an hour</h2>
+      <br/><br/><br/>
+      <div className='charts'>
+        <div className='chart-row'>
+          <CarbonChart metrics="memory/energy" title="Memory Energy"/>
+          <CarbonChart metrics="network/energy" title="Network Energy"/>
+        </div>
+        <div className='chart-row'>
+          <CarbonChart metrics="cpu/energy" title="CPU Energy"/>
+          <CarbonChart metrics= "grid/carbon-intensity" title="Grid Carbon Intensity"/>
+        </div>
+        <div className='chart-row'>
+          <CarbonChart metrics="energy" title="Energy"/>
+          <CarbonChart metrics="carbon-operational" title="Carbon Operational"/>
+        </div>
+        <CarbonChart metrics="cpu/thermal-design-power" title ="CPU Thermal Design Power"/>
       </div>
-      <div className='chart-row'>
-        <CarbonChart metrics="cpu/energy" title="CPU Energy"/>
-        <CarbonChart metrics= "grid/carbon-intensity" title="Grid Carbon Intensity"/>
-      </div>
-       <div className='chart-row'>
-        <CarbonChart metrics="energy" title="Energy"/>
-        <CarbonChart metrics="carbon-operational" title="Carbon Operational"/>
-      </div>
-      <CarbonChart metrics="cpu/thermal-design-power" title ="CPU Thermal Design Power"/>
-    </div>
+    </>
   )
 }
 
